b6b94ea84e168fcc3e64144148c55446cc98d279
Muhamad Farhan Noor Adabi <53655917+paankse@users.noreply.github.com>
2022-04-25 12:01:21 +0700

    First Upload Manually

---
+++ script.js
@@ -0,0 +1,5 @@
+function morefunc() {
+   var dgbatch2 = document.querySelector(".masonry-grid .batch2");
+   var morebtn = document.getElementsByClassName("more-btn");
+   dgbatch2.style.display = "inline";
+}
