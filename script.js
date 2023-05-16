adcc8563b5ede96b42c201acf8c3f0df4171b6e4
Muhamad Farhan Noor Adabi <53655917+paankse@users.noreply.github.com>
2023-05-16 13:34:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function show() {
-   // console.log("show");
-   document.getElementById("menu-frame").style.display = "flex";
-}
-
-function hide() {
-   document.getElementById("menu-frame").style.display = "none";
-}
